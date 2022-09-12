1ffaf6c831ccd04f64bc36866b941c2696c5b19b
navisraditya <navisraditya@gmail.com>
2022-09-12 12:37:11 +0700

    tugas2 done

--- katalog/views.py
+++ katalog/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
-def index(request):
-    return render(request, 'index.html')
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'list_barang': data_barang_katalog,
+        'nama': 'Muhammad Navis Raditya Riayatsyah',
+        'npm' : '2106717291'
+    }
+    
+    return render(request, 'katalog.html', context)
+    
+
