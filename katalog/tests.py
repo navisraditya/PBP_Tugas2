ba36a304ab2475a4bc66214ffff8f5dbca6bd95c
navisraditya <navisraditya@gmail.com>
2022-09-15 11:28:54 +0700

    tests.py modified

--- katalog/tests.py
+++ katalog/tests.py
@@ -1,9 +1,12 @@
-from django.test import TestCase
+from django.test import SimpleTestCase
 from django.urls import reverse, resolve
 from katalog.views import show_katalog
 
 # Create your tests here.
 class TestUrls(SimpleTestCase):
+    
     def test_show_catalog_url(self):
         url = reverse('katalog:show_katalog')
         self.assertEqual(resolve(url).func, show_katalog)
+
+# Source: https://www.youtube.com/watch?v=0MrgsYswT1c&list=PLbpAWbHbi5rMF2j5n6imm0enrSD9eQUaM&index=2
