894aa0014f35beffa4d56274b41b59944c2bd08a
navisraditya <navisraditya@gmail.com>
2022-09-28 21:58:19 +0700

    Tugas 4 done

--- todolist/models.py
+++ todolist/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+from email.policy import default
 from django.db import models
 from django.conf import settings
 from django.contrib.auth.models import User
@@ -6,7 +8,8 @@ from django.contrib.auth.models import User
 class todolist(models.Model):
     user = models.ForeignKey(
         User,
-        on_delete = models.CASCADE,
+        on_delete = models .CASCADE,
     )
     title = models.TextField()
+    date = models.DateTimeField(default=datetime.now)
     description = models.TextField()
