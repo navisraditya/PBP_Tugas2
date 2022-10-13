726f2a67a12c59c327f98aa81920b903be68adc1
navisraditya <navisraditya@gmail.com>
2022-10-13 09:08:33 +0700

    Tugas 6; modal not working

--- todolist/forms.py
+++ todolist/forms.py
@@ -6,5 +6,5 @@ from .models import todolist
 class AddList(ModelForm):
     class Meta:
         model = todolist
-        exclude = ('user', 'is_finished')
+        exclude = ('is_finished',)
         fields = '__all__'
